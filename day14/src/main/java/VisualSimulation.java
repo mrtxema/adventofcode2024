3562afe07bb9dd2fbd220fc0d9f8ac1db0d2ce1c
Txema Martinez <mrtxema@gmail.com>
2024-12-15 10:33:52 +0100

    Improve visual simulation

--- day14/src/main/java/VisualSimulation.java
+++ day14/src/main/java/VisualSimulation.java
@@ -2,19 +2,23 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.stream.IntStream;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
-public class VisualSimulation extends JPanel {
+public class VisualSimulation extends JPanel implements ActionListener {
     private static final String INPUT_FILE_NAME = "input.txt";
     private static final int WIDTH = 101;
     private static final int HEIGHT = 103;
     private static final int PIXEL_SIZE = 8;
     private final RobotMap robotMap;
+    private final Timer timer;
+    private final int easterEggSeconds;
     private int movements = 0;
 
     public static void main(String[] args) {
@@ -31,34 +35,44 @@ public class VisualSimulation extends JPanel {
 
     public VisualSimulation() {
         this.robotMap = new Solver(INPUT_FILE_NAME, WIDTH, HEIGHT).parseFile().getRobotMap();
+        this.easterEggSeconds = robotMap.findEasterEggSeconds();
+        this.timer = new Timer(10, this);
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) {
-                SwingUtilities.invokeLater(() -> findTree());
-            }
-        });
+        timer.start();
     }
 
     public Dimension getPreferredSize() {
         return new Dimension(WIDTH * PIXEL_SIZE,HEIGHT * PIXEL_SIZE);
     }
 
-    private void findTree() {
-        movements = robotMap.findEasterEggSeconds();
-        robotMap.simulateMovements(movements);
-        repaint();
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==timer && movements < easterEggSeconds) {
+            robotMap.simulateMovements(++movements);
+            repaint();
+        }
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        robotMap.getRobots().forEach(robot -> paintRobot(g, robot));
+        IntStream.range(0, robotMap.getRobots().size()).forEach(i -> paintRobot(g, robotMap.getRobots().get(i), i));
+        g.setColor(Color.WHITE);
+        g.fillRect(15, 5, 200, 30);
         g.setColor(Color.BLACK);
         g.setFont(new Font("TimesRoman", Font.BOLD, 20));
-        g.drawString("Movements: " + movements, 20, 20);
+        g.drawString("Movements: " + movements, 30, 30);
     }
 
-    private void paintRobot(Graphics g, Robot robot) {
-        g.setColor(Color.RED);
+    private void paintRobot(Graphics g, Robot robot, int robotNumber) {
+        var color = movements == easterEggSeconds ? Color. decode("#006600") : getRobotColor(robotNumber);
+        g.setColor(color);
         g.fillRect(robot.position().x() * PIXEL_SIZE, robot.position().y() * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+    }
+
+    private Color getRobotColor(int robotNumber) {
+        int red = ((robotNumber >> 6) & 0x7) * 0x22;
+        int green = ((robotNumber >> 3) & 0x7) * 0x22;
+        int blue = (robotNumber & 0x7) * 0x22;
+        return new Color(red, green, blue);
     }
 }
